b8c2b848f1a222f9174055a092246b1a3c9d424a
BadhanCB <badhancsesastc112@gmail.com>
2023-01-12 14:47:41 +0600

    organize all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Topbar from "./components/Topbar/Topbar";
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Topbar />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Topbar from '../../components/Topbar/Topbar';
+
+const Home = () => {
+    return (
+        <main>
+            <Topbar />
+        </main>
+    );
+};
+
+export default Home;
